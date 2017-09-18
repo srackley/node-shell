2a5a7ac8daae09e8f652b9e4c4506f2b5efc4d7c
Shelby Rackley <shelbyrackley@Shelbys-MacBook-Pro.local>
2017-09-18 12:59:57 -0400

    all done

--- bash.js
+++ bash.js
@@ -4,6 +4,13 @@ const dateCheck = 'date';
 const ls = 'ls';
 const echo = 'echo';
 const cat = 'cat';
+const head = 'head';
+const tail = 'tail';
+const sort = 'sort';
+const wc = 'wc';
+const uniq = 'uniq';
+const curl = 'curl';
+const find = 'find';
 
 process.stdout.write('prompt > ');
 
@@ -22,8 +29,27 @@ commands[ls]();
     commands[echo](arr.slice(1).join(' '));
   } else if (arr[0] == 'cat'){
     commands[cat](arr.slice(1).join(' '));
-  } else {
+  } else if (arr[0] == 'head'){
+    commands[head](arr[1]);
+  } else if (arr[0] == 'tail'){
+    commands[tail](arr[1]);
+   } else if (arr[0] == 'sort'){
+     commands[sort](arr[1]);
+  } else if (arr[0] == 'wc'){
+    commands[wc](arr[1]);
+  } else if (arr[0] == 'uniq'){
+    commands[uniq](arr[1]);
+   } else if (arr[0] == 'curl'){
+    commands[curl](arr[1]);
+  } else if (arr[0] == 'find'){
+    commands[curl](arr[1]);
+      } else {
 process.stdout.write('You typed: ' + file + '\n');
 console.log(arr);
   }
 });
+
+var done = function(output){
+  process.stdout.write(output);
+  process.stdout.write('prompt > ');
+}
--- commands.js
+++ commands.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const output = process.stdout;
-
+const req = require('request');
 module.exports = {
     pwd: function(){
             console.log(process.cwd());  
@@ -30,6 +30,71 @@ module.exports = {
             if (err) throw err;
            output.write(data + '\n');
             output.write('prompt > ');  
+        });
+    },
+
+    head: function(file){
+        fs.readFile(file, function(err, data){
+            if (err) throw err;
+            const lines = data.toString();
+            const arr = lines.split('\n', 5).join('\n');
+        output.write(arr);
+            
+        })
+    },
+
+    tail: function(file){
+        fs.readFile(file, function(err, data){
+            if (err) throw err;
+            const lines = data.toString();
+            const arr = lines.split('\n');
+            const str = arr.slice(arr.length - 5).join('\n')
+           output.write(str);
+            
+        })
+    },
+
+    sort: function(file){
+        fs.readFile(file, function(err, data){
+            if (err) throw err;
+            const lines = data.toString();
+            const arr = lines.split('\n');
+            const str = arr.sort().join('\n');
+        return str;
+            //fix uppercase/lowercase problem if we have time
+        })
+    },
+
+    wc: function(file){
+        fs.readFile(file, function(err, data){
+            if (err) throw err;
+            const lines = data.toString();
+            const arr = lines.split('\n');
+        output.write(arr.length.toString());
+        });
+    },
+
+    uniq: function(file){
+        fs.readFile(file, function(err, data){
+            if (err) throw err;
+            const newArr = [];
+            const lines = data.toString();
+            const arr = lines.split('\n').sort();
+            for (var i = 0; i < arr.length; i++){
+                if (arr[i] !== arr[i + 1]){
+                    newArr.push(arr[i]);
+                }
+            }
+            const str = newArr.join('\n')
+            output.write(str);
+        });
+    }, 
+
+    curl: function(url){
+        req(url, function(err, resp, body){
+            console.log('error:', err);
+            console.log('statusCode:', resp && resp.statusCode);
+            console.log('body:', body);
         });
     }
 }
