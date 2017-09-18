93bf76d2abb3a04f58bb24a12875a667647bd925
Shelby Rackley <shelbyrackley@Shelbys-MacBook-Pro.local>
2017-09-18 11:17:25 -0400

    cat working

--- bash.js
+++ bash.js
@@ -2,20 +2,28 @@ const commands = require('./commands');
 const userCommand = 'pwd';
 const dateCheck = 'date';
 const ls = 'ls';
+const echo = 'echo';
+const cat = 'cat';
 
 process.stdout.write('prompt > ');
 
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function (data) {
-    
-  const cmd = data.toString().trim();
-  if (cmd == 'pwd'){
-commands[userCommand]();
-  } else if (cmd == 'date'){
-commands[dateCheck]();
-  } else if (cmd == 'ls'){
+  
+  const file = data.toString().trim();
+  const arr = file.split(' ');
+  if (file == 'pwd'){
+commands[userCommand](file);
+  } else if (file == 'date'){
+commands[dateCheck](file);
+  } else if (file == 'ls'){
 commands[ls]();
+  } else if (arr[0] == 'echo'){
+    commands[echo](arr.slice(1).join(' '));
+  } else if (arr[0] == 'cat'){
+    commands[cat](arr.slice(1).join(' '));
   } else {
-process.stdout.write('You typed: ' + cmd + '\n');
+process.stdout.write('You typed: ' + file + '\n');
+console.log(arr);
   }
 });
--- commands.js
+++ commands.js
@@ -2,17 +2,17 @@ const fs = require('fs');
 const output = process.stdout;
 
 module.exports = {
-    pwd: function(cwd){
+    pwd: function(){
             console.log(process.cwd());  
             output.write('prompt > ');            
     },
-    date: function(cwd){
+    date: function(file){
         const d = new Date();
         console.log(d.toString()); 
         output.write('prompt > ');
     },
 
-    ls: function(){
+    ls: function(file){
         fs.readdir('.', function(err, files){
             if (err) throw err;
             files.forEach(function(file){
@@ -20,7 +20,18 @@ module.exports = {
             })
             output.write('prompt > ');  
         });
-    }    
+    }, 
+    echo: function(file){
+        console.log(file);
+    },
+
+    cat: function(file){
+        fs.readFile(file, function(err, data){
+            if (err) throw err;
+           output.write(data + '\n');
+            output.write('prompt > ');  
+        });
+    }
 }
 
 //fs.readFile(something, 'utf8', function(err, contents){
